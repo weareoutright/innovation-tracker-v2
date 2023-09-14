2505f109d275a3674721cde3e5ebd12a415eb874
Dakota Fabro <dakota@weareoutright.com>
2023-09-14 17:47:18 -0500

    Import and modify svg files for budget pie chart

--- src/BudgetChart.js
+++ src/BudgetChart.js
@@ -1,33 +1,11 @@
+import {
+  fy2021ModSvg,
+  fy2022ModSvg,
+  fy2023ModSvg,
+} from "./constants/budgetPieCharts";
 
-const BudgetChart = props => {
-  return (
-    <svg viewBox="0 0 755 1136.8" x="0" y="0" className="wells-chart-budget">
-      <path className="st2" d="M529.5,533.4C628,533.4,672,144,672,57.6c0,90.5,0,1019.9,0,1019.9s-40.7-476.7-142.4-476.7
-        C529.6,567.5,529.5,533.4,529.5,533.4z"/>
-      <g>
-        <g>
-          <g>
-            <g>
-              <linearGradient id="SVGID_1_" gradientUnits="userSpaceOnUse" x1="414.2364" y1="582.4072" x2="527.1773" y2="537.2308">
-                <stop  offset="0.195" style={{'stopColor':'#253D86'}} />
-                <stop  offset="0.8827" style={{'stopColor':'#9C5FB5'}} />
-              </linearGradient>
-              <path className="st3" d="M290.3,567.5l239.3-34.1c3.2,22.5,3.2,44.8,0.1,67.3L290.3,567.5z"/>
-            </g>
-          </g>
-          <g>
-            <g>
-              <path className="st4" d="M290.3,567.5l239.4,33.2c-18.3,132.2-140.4,224.5-272.6,206.2S32.5,666.6,50.9,534.3
-                s140.4-224.5,272.6-206.2c109.6,15.2,190.5,95.8,206.1,205.3L290.3,567.5z"/>
-            </g>
-          </g>
-        </g>
-        <circle className="st5" cx="290.3" cy="567.5" r="137.8"/>
-        <text transform="matrix(1 0 0 1 244.9233 505.508)"><tspan x="0" y="0" className="st6 st7">FY{props.shortYear} </tspan><tspan x="-21.6" y="42" className="st6 st7">Budget</tspan></text>
-        <text transform="matrix(1 0 0 1 210.6974 591.0551)"><tspan x="0" y="0" className="st8 st9">of considered </tspan><tspan x="-9.2" y="30" className="st8 st9">agencies (DOE, </tspan><tspan x="8.7" y="60" className="st8 st9">DOT, USDA)</tspan></text>
-      </g>
-    </svg>
-  )
-}
+const BudgetChart = ({ shortYear }) => {
+  return <>{fy2023ModSvg}</>;
+};
 
 export default BudgetChart;
