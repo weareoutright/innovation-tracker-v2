15c38436c6ca06d28ad7baef1c9dc794169c8cf6
Dakota Fabro <dakota@weareoutright.com>
2023-10-04 16:32:44 -0700

    Build with new data

--- src/constants/sources.js
+++ src/constants/sources.js
@@ -1,6 +1,6 @@
 const sources = {
-  data_prod: "/wp-content/uploads/2022/11/master_v2.1.csv", //prod
-  data_dev: "/data/master_v2.1.csv", //dev
+  data_prod: "/wp-content/uploads/2022/11/master_v2_1.csv", //prod
+  data_dev: "/data/master_v2_1.csv", //dev
   download:
     "/wp-content/uploads/2022/11/EDF-Climate-Innovation-Tracker-Data-9_19_23.xlsx",
 };
