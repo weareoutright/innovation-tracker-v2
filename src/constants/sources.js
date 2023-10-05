4b65b9896bf2749ef58c1ccb2a9f7f59c090a1f9
Dakota Fabro <dakota@weareoutright.com>
2023-10-04 17:12:00 -0700

    Build for new download .xlsx

--- src/constants/sources.js
+++ src/constants/sources.js
@@ -2,7 +2,7 @@ const sources = {
   data_prod: "/wp-content/uploads/2022/11/master_v2_1.csv", //prod
   data_dev: "/data/master_v2_1.csv", //dev
   download:
-    "/wp-content/uploads/2022/11/EDF-Climate-Innovation-Tracker-Data-9_19_23.xlsx",
+    "/wp-content/uploads/2022/11/EDF-Climate-Innovation-Tracker-Data-10_4_23.xlsx",
 };
 
 export default sources;
