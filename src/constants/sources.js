c751688ade189a4a61d0fc28d295c1306a588cff
Dakota Fabro <dakota@weareoutright.com>
2023-10-06 11:49:28 -0700

    Build & Rename data to master.csv

--- src/constants/sources.js
+++ src/constants/sources.js
@@ -1,6 +1,6 @@
 const sources = {
-  data_prod: "/wp-content/uploads/2022/11/master_v2_1.csv",
-  data_dev: "/data/master_v2_1.csv",
+  data_prod: "/wp-content/uploads/2022/11/master.csv",
+  data_dev: "/data/master.csv",
   download:
     "/wp-content/uploads/2022/11/EDF-Climate-Innovation-Tracker-Data-10_4_23.xlsx",
 };
