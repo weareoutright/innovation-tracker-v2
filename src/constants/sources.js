00a442e6b60a092ef833fddc410bf21bb42ccdf4
Dakota Fabro <dakota@weareoutright.com>
2023-10-11 21:38:39 -0700

    Added comment regarding keeping file paths the same

--- src/constants/sources.js
+++ src/constants/sources.js
@@ -1,3 +1,7 @@
+/**
+ * ! Do not change path names. If updating a file, update file name to reflect path name instead.
+ */
+
 const sources = {
   data_prod: "/wp-content/uploads/2022/11/master.csv",
   data_dev: "/data/master.csv",
