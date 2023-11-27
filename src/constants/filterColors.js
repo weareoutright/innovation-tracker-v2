91ac67cce66ed4e6cdbc53f02d264274169c6ace
Dakota Fabro <dakota@weareoutright.com>
2023-11-27 13:06:08 -0800

    Update fonts and filter colors for sankey

--- src/constants/filterColors.js
+++ src/constants/filterColors.js
@@ -1,6 +1,4 @@
 /* TODO for 11/22: 
-  - replace brand colors in this file
-  - replace brand colors in scss files
   - adjust tracker to be aligned in the center (since budget chart has been removed)
   - check breakpoints (see PAIC breakpoints - take note of this and document the breakpoints somewhere - share with Gray)
   - Add logic to gray out filters being used OR create a button with a z-index of -1 under the filter and disable the button. This way, when the filter is dragged, the grayed out button is left behind
@@ -10,10 +8,10 @@
 */
 
 export const filterColors = {
-  stage: "#029EDA",
-  sector: "#8BD2C1",
-  solution: "#EBB700",
-  funding_type: "#C8DA2C",
-  funding_source: "#9C5FB5",
-  default: "#1D5C42",
+  stage: "#A1E214",
+  sector: "#009933",
+  solution: "#B76CD9",
+  funding_type: "#33CCFF",
+  funding_source: "#FFD83A",
+  default: "#0033CC",
 };
