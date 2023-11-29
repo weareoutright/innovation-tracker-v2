7958a64168d4f5b2fa422c797752b031be7d2ffd
Dakota Fabro <dakota@weareoutright.com>
2023-11-28 19:55:50 -0800

    Update app header

--- src/Footer.js
+++ src/Footer.js
@@ -11,8 +11,8 @@ import useWindowSize from "./useWindowSize";
 
 const Footer = ({ handleYearChange, dataYears, shortYear }) => {
   const { width } = useWindowSize();
-  console.log(width);
 
+  // WORKING ON MOBILE
   if (width > BREAKPOINTS.md) {
     return (
       <footer className="app-footer">
@@ -27,6 +27,22 @@ const Footer = ({ handleYearChange, dataYears, shortYear }) => {
         </div>
       </footer>
     );
+  } else if (width <= BREAKPOINTS.md && width > 400) {
+    return (
+      <footer className="app-footer">
+        <div className="footer__wrapper">
+          <DraggableFilters filters={groups} />
+          <div className="footer-second-row-md-breakpoint">
+            <DataYearSelect
+              handleYearChange={handleYearChange}
+              dataYears={dataYears}
+            />
+            <DownloadTrigger />
+            <Stickies shortYear={shortYear} />
+          </div>
+        </div>
+      </footer>
+    );
   } else {
     return (
       <footer className="app-footer">
@@ -37,6 +53,8 @@ const Footer = ({ handleYearChange, dataYears, shortYear }) => {
               handleYearChange={handleYearChange}
               dataYears={dataYears}
             />
+          </div>
+          <div className="footer-third-row-sm-breakpoint">
             <DownloadTrigger />
             <Stickies shortYear={shortYear} />
           </div>
--- src/Header.js
+++ src/Header.js
@@ -6,11 +6,14 @@ import AgencyLevelContext from "./context/AgencyLevelContext";
 import HelpersContext from "./context/HelpersContext";
 import { filterColors } from "./constants/filterColors.js";
 import { mappedGroups } from "./constants/groups.js";
+import useWindowSize from "./useWindowSize.js";
+import { BREAKPOINTS } from "./constants/breakpoints.js";
 
 import utils from "./utils";
 import labels from "./constants/labels";
 
 const Header = ({ shortYear }) => {
+  const { width } = useWindowSize();
   const { graph, setGraph } = useContext(GraphContext);
   const { active, setActive } = useContext(ActiveContext);
   const { selected, setSelected } = useContext(SelectedContext);
@@ -33,85 +36,171 @@ const Header = ({ shortYear }) => {
     setShowHelpers(false);
   };
 
-  return (
-    <header
-      className="app-header"
-      style={{
-        borderBottom:
-          active[0] === null && active[0] === null ? "1px solid #fff" : "",
-      }}
-    >
-      <div className="component-well-headers">
-        <div className="well__headers">
-          {active.map((well, w) => {
-            return (
-              <React.Fragment key={w}>
-                <div
-                  className={`well__header${
-                    well !== null ? " well__header-active" : ""
-                  }`}
-                  key={w}
-                  onClick={() => closeHeader(w)}
-                >
-                  {well !== null && (
-                    <h3>
-                      <span
-                        style={{
-                          border: `1px solid ${filterColors[well]}`,
-                          backgroundColor: `${filterColors[well]}`,
-                          color:
-                            well === "agency" ||
-                            well === "solution" ||
-                            well === "sector"
-                              ? "#fff"
-                              : "#000",
-                        }}
-                      >
-                        {labels.getLabel(well)}{" "}
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          viewBox="0 0 48 48"
+  if (width > BREAKPOINTS.md) {
+    return (
+      <header
+        className="app-header"
+        style={{
+          borderBottom:
+            active[0] === null && active[0] === null ? "1px solid #fff" : "",
+        }}
+      >
+        <div className="component-well-headers">
+          <div className="well__headers">
+            {active.map((well, w) => {
+              return (
+                <React.Fragment key={w}>
+                  <div
+                    className={`well__header${
+                      well !== null ? " well__header-active" : ""
+                    }`}
+                    key={w}
+                    onClick={() => closeHeader(w)}
+                  >
+                    {well !== null && (
+                      <h3>
+                        <span
+                          style={{
+                            border: `1px solid ${filterColors[well]}`,
+                            backgroundColor: `${filterColors[well]}`,
+                            color:
+                              well === "agency" ||
+                              well === "solution" ||
+                              well === "sector"
+                                ? "#fff"
+                                : "#000",
+                          }}
                         >
-                          <path d="m12.45 37.65-2.1-2.1L21.9 24 10.35 12.45l2.1-2.1L24 21.9l11.55-11.55 2.1 2.1L26.1 24l11.55 11.55-2.1 2.1L24 26.1Z" />
-                        </svg>
-                      </span>
-                      <em>
-                        {selected[w]
-                          ? utils.getTruncatedWords(
-                              labels.getLabel(selected[w])
-                            )
-                          : "All"}
-                      </em>
-                    </h3>
-                  )}
-                </div>
-                <React.Fragment>
-                  {w == 0 && anyActive && (
-                    <div className="well__header well__header-active well__header-main">
-                      <h2>
-                        {anySelected
-                          ? `Selected FY${shortYear} Climate Innovation Funding: Mitigation`
-                          : `FY${shortYear} Climate Innovation Funding: Mitigation`}
+                          {labels.getLabel(well)}{" "}
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 48 48"
+                          >
+                            <path d="m12.45 37.65-2.1-2.1L21.9 24 10.35 12.45l2.1-2.1L24 21.9l11.55-11.55 2.1 2.1L26.1 24l11.55 11.55-2.1 2.1L24 26.1Z" />
+                          </svg>
+                        </span>
                         <em>
-                          {amount} ({percentage})
+                          {selected[w]
+                            ? utils.getTruncatedWords(
+                                labels.getLabel(selected[w])
+                              )
+                            : "All"}
                         </em>
-                        {/* need to look into which subgroups to map here -- how to access?
+                      </h3>
+                    )}
+                  </div>
+                  <React.Fragment>
+                    {w == 0 && anyActive && (
+                      <div className="well__header well__header-active well__header-main">
+                        <h2>
+                          {anySelected
+                            ? `Selected FY${shortYear} Climate Innovation Funding: Mitigation`
+                            : `FY${shortYear} Climate Innovation Funding: Mitigation`}
+                          <em>
+                            {amount} ({percentage})
+                          </em>
+                          {/* need to look into which subgroups to map here -- how to access?
                         <em>
                           {mappedGroups[well].map((group) =>
                             labels.getLabel(group)
                           )}
                         </em> */}
-                      </h2>
-                    </div>
-                  )}
+                        </h2>
+                      </div>
+                    )}
+                  </React.Fragment>
                 </React.Fragment>
-              </React.Fragment>
-            );
-          })}
+              );
+            })}
+          </div>
         </div>
-      </div>
-    </header>
-  );
+      </header>
+    );
+  } else if (width <= 400) {
+    return (
+      <header
+        className="app-header"
+        style={{
+          borderBottom:
+            active[0] === null && active[0] === null ? "1px solid #fff" : "",
+        }}
+      >
+        <div className="component-well-headers">
+          <div className="well__headers">
+            <React.Fragment>
+              {/* {w == 0 && anyActive && ( */}
+              <div className="well__header well__header-active well__header-main">
+                <h2>
+                  {anySelected
+                    ? `Selected FY${shortYear} Climate Innovation Funding: Mitigation`
+                    : `FY${shortYear} Climate Innovation Funding: Mitigation`}
+                  <em>
+                    {amount} ({percentage})
+                  </em>
+                  {/* need to look into which subgroups to map here -- how to access?
+                        <em>
+                          {mappedGroups[well].map((group) =>
+                            labels.getLabel(group)
+                          )}
+                        </em> */}
+                </h2>
+              </div>
+              {/* )} */}
+            </React.Fragment>
+            <div className="second-header-row-sm">
+              {active.map((well, w) => {
+                return (
+                  <>
+                    <React.Fragment key={w}>
+                      <div
+                        className={`well__header${
+                          well !== null ? " well__header-active" : ""
+                        }`}
+                        key={w}
+                        onClick={() => closeHeader(w)}
+                      >
+                        {well !== null && (
+                          <h3>
+                            <span
+                              style={{
+                                border: `1px solid ${filterColors[well]}`,
+                                backgroundColor: `${filterColors[well]}`,
+                                color:
+                                  well === "agency" ||
+                                  well === "solution" ||
+                                  well === "sector"
+                                    ? "#fff"
+                                    : "#000",
+                              }}
+                            >
+                              {labels.getLabel(well)}{" "}
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 48 48"
+                              >
+                                <path d="m12.45 37.65-2.1-2.1L21.9 24 10.35 12.45l2.1-2.1L24 21.9l11.55-11.55 2.1 2.1L26.1 24l11.55 11.55-2.1 2.1L24 26.1Z" />
+                              </svg>
+                            </span>
+                            <em>
+                              {selected[w]
+                                ? utils.getTruncatedWords(
+                                    labels.getLabel(selected[w])
+                                  )
+                                : "All"}
+                            </em>
+                          </h3>
+                        )}
+                      </div>
+                    </React.Fragment>
+                  </>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      </header>
+    );
+  }
 };
 
 export default Header;
